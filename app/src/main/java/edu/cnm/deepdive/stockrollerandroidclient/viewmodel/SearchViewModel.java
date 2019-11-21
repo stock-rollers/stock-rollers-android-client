9bcb30200b55f73895881966d58272a7ac5c94ff
Khasiano <NewbieCoder10>
2019-11-21 12:42:23 -0700

    Created a Search fragment, along with the xml and the viewModel that corresponds with it.

--- app/src/main/java/edu/cnm/deepdive/stockrollerandroidclient/viewmodel/SearchViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/stockrollerandroidclient/viewmodel/SearchViewModel.java
@@ -1,5 +1,23 @@
 package edu.cnm.deepdive.stockrollerandroidclient.viewmodel;
 
-public class SearchViewModel {
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import edu.cnm.deepdive.stockrollerandroidclient.service.StockRollersDatabase;
 
+public class SearchViewModel extends AndroidViewModel {
+
+
+  private StockRollersDatabase database;
+  private MutableLiveData<Long> stockId = new MutableLiveData<>();
+//  private LiveData<Stock> stock;
+
+  public SearchViewModel(@NonNull Application application) {
+    super(application);
+//    database = StockRollersDatabase.getInstance();
+//    stock = Transformations.switchMap(stockId, (id) -> database.getStockRepository)
+  }
 }
